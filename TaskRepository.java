2fd32b699234a3533338f0342f850105079225a2
Ciro Aiello <ciroaiello@live.com>
2025-02-06 13:34:06 +0100

    Aggiunto codice generato per development 1

---
+++ TaskRepository.java
@@ -0,0 +1,11 @@
+```java
+package com.example.taskflow.repository;
+
+import com.example.taskflow.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
+```
