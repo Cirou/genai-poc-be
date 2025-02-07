b8c17a55ceda8e25e573df0bc04a8df7f48dda55
Ciro Aiello <ciroaiello@live.com>
2025-02-07 15:28:32 +0100

    Aggiornato codice generato per development 1

--- src/main/java/com/example/taskflow/controller/TaskController.java
+++ src/main/java/com/example/taskflow/controller/TaskController.java
@@ -1,15 +1,30 @@
 package com.example.taskflow.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.taskflow.model.Task;
+import com.example.taskflow.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @GetMapping
-    public String getAllTasks() {
-        return "List of tasks";
+    public List<Task> getAllTasks() {
+        return taskService.getTasks();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
     }
 }
