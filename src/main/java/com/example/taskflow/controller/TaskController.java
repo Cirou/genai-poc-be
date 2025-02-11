79630c4df9973b1de55b689fae8b220ef2fbd24f
Ciro Aiello <ciroaiello@live.com>
2025-02-11 18:56:45 +0100

    Aggiornato codice generato per development 1

--- src/main/java/com/example/taskflow/controller/TaskController.java
+++ src/main/java/com/example/taskflow/controller/TaskController.java
@@ -1,15 +1,31 @@
 package com.example.taskflow.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.taskflow.dto.TaskDTO;
+import com.example.taskflow.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
+    @Autowired
+    private TaskService taskService;
+
     @GetMapping
-    public String getAllTasks() {
-        return "List of tasks";
+    public ResponseEntity<List<TaskDTO>> getAllTasks() {
+        List<TaskDTO> tasks = taskService.getAllTasks();
+        return ResponseEntity.ok(tasks);
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskDTO> createTask(@Valid @RequestBody TaskDTO taskDTO) {
+        TaskDTO createdTask = taskService.createTask(taskDTO);
+        return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
     }
 }
