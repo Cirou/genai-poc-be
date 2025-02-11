2389c8fd698865908fb6f50879f5fe3f5e3c2d5d
Ciro Aiello <ciroaiello@live.com>
2025-02-11 18:56:45 +0100

    Aggiornato codice generato per development 1

--- src/main/java/com/example/taskflow/config/AppConfig.java
+++ src/main/java/com/example/taskflow/config/AppConfig.java
@@ -1,7 +1,9 @@
 package com.example.taskflow.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class AppConfig {
 }
